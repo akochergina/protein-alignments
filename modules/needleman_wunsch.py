df0433b5ffcb68e3cc81205c4ed1d1c94d61cda7
Alena Kochergina <alena.kochergina@polytechnique.edu>
2025-01-28 15:32:46 +0100

    project structure added

---
+++ modules/needleman_wunsch.py
@@ -0,0 +1,18 @@
+def needleman_wunsch(seq1, seq2):
+    """
+    Perform Needleman-Wunsch alignment.
+
+    Parameters:
+    ----------
+    seq1 : str
+        First sequence to align.
+    seq2 : str
+        Second sequence to align.
+
+    Returns:
+    -------
+    alignment : tuple
+        A tuple containing the aligned sequences.
+    """
+    # Implementation here
+    pass
