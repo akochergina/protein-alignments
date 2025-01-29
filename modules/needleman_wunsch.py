9889576dfc7590ef7dba0c98a9ce207d2a541bb7
Alena Kochergina <alena.kochergina@polytechnique.edu>
2025-01-29 22:24:30 +0100

    extension of needleman wunsch alg with different cost of gap opening and gap extension

--- modules/needleman_wunsch.py
+++ modules/needleman_wunsch.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-def score_i_j_alignment(i: chr, j: chr, identity_score, substitution_score, gap_score):
+def score_i_j_alignment(i: chr, j: chr, identity_score, substitution_score):
     """
     Calculate the score of aligning characters i and j.
 
@@ -17,8 +17,6 @@ def score_i_j_alignment(i: chr, j: chr, identity_score, substitution_score, gap_
         Score for aligning identical characters.
     substitution_score : int
         Score for aligning non-identical characters.
-    gap_score : int
-        Score for introducing a gap.
 
     Returns:
     -------
@@ -27,70 +25,9 @@ def score_i_j_alignment(i: chr, j: chr, identity_score, substitution_score, gap_
     """
     if i == j:
         return identity_score
-    elif i == '-' or j == '-':
-        return gap_score
     else:
         return substitution_score
 
-def fill_needleman_wunsch_matrix(seq1, seq2, identity_score, substitution_score, gap_score):
-    '''
-    Fill the Needleman-Wunsch matrix and store the indexes of the arrows with possibility to have up to 3 arrows.
-
-    Parameters:
-    ----------
-    seq1 : str
-        First sequence to align.
-    seq2 : str
-        Second sequence to align.
-    identity_score : int
-        Score for aligning identical characters.
-    substitution_score : int
-        Score for aligning non-identical characters.
-    gap_score : int
-        Score for introducing a gap.
-
-    Returns:
-    -------
-    matrix : pandas.DataFrame
-        The filled matrix.
-    '''
-    # Initialize the matrix
-    rows = len(seq1) + 1
-    cols = len(seq2) + 1
-    matrix = pandas.DataFrame(index=range(rows), columns=range(cols))
-    arrow_matrix = pandas.DataFrame(index=range(rows), columns=range(cols))
-
-    arrow_matrix.at[0, 0] = None
-    matrix.at[0, 0] = 0
-
-    # Initialize the first row and column
-    for i in range(1, rows):
-        matrix.at[i, 0] = i * gap_score
-        arrow_matrix.at[i, 0] = [(i-1, 0)]
-    for j in range(1, cols):
-        matrix.at[0, j] = j * gap_score
-        arrow_matrix.at[0, j] = [(0, j-1)]
-
-    # Fill the matrix 
-    for i in range(1, rows):
-        for j in range(1, cols):
-            # Calculate the scores
-            match = matrix.at[i-1, j-1] + score_i_j_alignment(seq1[i-1], seq2[j-1], identity_score, substitution_score, gap_score)
-            delete = matrix.at[i-1, j] + gap_score
-            insert = matrix.at[i, j-1] + gap_score
-
-            # Update the matrix
-            max_score = max(match, delete, insert)
-            arrow_matrix.at[i, j] = []
-            matrix.at[i, j] = max_score
-            if max_score == match:
-                arrow_matrix.at[i, j].append((i-1, j-1))
-            if max_score == delete:
-                arrow_matrix.at[i, j].append((i-1, j))
-            if max_score == insert:
-                arrow_matrix.at[i, j].append((i, j-1))
-
-    return matrix, arrow_matrix
 
 def plot_nw_matrix(matrix, arrow_matrix, seq1, seq2):
     """
@@ -146,7 +83,7 @@ def plot_nw_matrix(matrix, arrow_matrix, seq1, seq2):
                         ax.arrow(j + 0.5, i + 0.35, dx * 0.5, dy * 0.5, head_width=0.1, 
                                  head_length=0.1, fc=color, ec=color)
                     elif dy == 0:
-                        ax.arrow(j + 0.35, i + 0.5, dx * 0.5, dy * 0.5, head_width=0.1, 
+                        ax.arrow(j + 0.25, i + 0.5, dx * 0.4, dy * 0.5, head_width=0.1, 
                                  head_length=0.1, fc=color, ec=color)
                     else:
                         ax.arrow(j + 0.35, i + 0.35, dx * 0.6, dy * 0.6, head_width=0.1, 
@@ -177,7 +114,81 @@ def print_nw_result(matrix, arrow_matrix, score, alignment1, alignment2, seq1, s
     plot_nw_matrix(matrix, arrow_matrix, seq1, seq2)
     return
 
-def needleman_wunsch(seq1, seq2, identity_score, substitution_score, gap_score):
+def fill_needleman_wunsch_matrix(seq1, seq2, identity_score, substitution_score, gap_opening_score, gap_extension_score):
+    '''
+    Fill the Needleman-Wunsch matrix and store the indexes of the arrows with possibility to have up to 3 arrows.
+
+    Parameters:
+    ----------
+    seq1 : str
+        First sequence to align.
+    seq2 : str
+        Second sequence to align.
+    identity_score : int
+        Score for aligning identical characters.
+    substitution_score : int
+        Score for aligning non-identical characters.
+    gap_score : int
+        Score for introducing a gap.
+
+    Returns:
+    -------
+    matrix : pandas.DataFrame
+        The filled matrix.
+    '''
+    # Initialize the matrix
+    rows = len(seq1) + 1
+    cols = len(seq2) + 1
+    matrix = pandas.DataFrame(index=range(rows), columns=range(cols))
+    arrow_matrix = pandas.DataFrame(index=range(rows), columns=range(cols))
+    gap_matrix = pandas.DataFrame(index=range(rows), columns=range(cols))
+
+    arrow_matrix.at[0, 0] = None
+    matrix.at[0, 0] = 0
+    gap_matrix.at[0, 0] = 0
+ 
+
+    # Initialize the first row and column
+    for i in range(1, rows):
+        matrix.at[i, 0] = gap_opening_score + (i-1) * gap_extension_score
+        arrow_matrix.at[i, 0] = [(i-1, 0)]
+        gap_matrix.at[i, 0] = 1
+    for j in range(1, cols):
+        matrix.at[0, j] = gap_opening_score + (j-1) * gap_extension_score
+        arrow_matrix.at[0, j] = [(0, j-1)]
+        gap_matrix.at[0, j] = 1
+
+    # Fill the matrix 
+    for i in range(1, rows):
+        for j in range(1, cols):
+            # Calculate the scores
+            match = matrix.at[i-1, j-1] + score_i_j_alignment(seq1[i-1], seq2[j-1], identity_score, substitution_score)
+            if gap_matrix.at[i-1, j] == 1:
+                delete = matrix.at[i-1, j] + gap_extension_score
+            else:
+                delete = matrix.at[i-1, j] + gap_opening_score
+            if gap_matrix.at[i, j-1] == 1:
+                insert = matrix.at[i, j-1] + gap_extension_score
+            else:
+                insert = matrix.at[i, j-1] + gap_opening_score
+
+            # Update the matrix
+            max_score = max(match, delete, insert)
+            arrow_matrix.at[i, j] = []
+            matrix.at[i, j] = max_score
+            if max_score == match:
+                arrow_matrix.at[i, j].append((i-1, j-1))
+                gap_matrix.at[i, j] = 0
+            if max_score == delete:
+                arrow_matrix.at[i, j].append((i-1, j))
+                gap_matrix.at[i, j] = 1
+            if max_score == insert:
+                arrow_matrix.at[i, j].append((i, j-1))
+                gap_matrix.at[i, j] = 1
+
+    return matrix, arrow_matrix
+
+def needleman_wunsch(seq1, seq2, identity_score, substitution_score, gap_opening_score, gap_extension_score):
     """
     Perform Needleman-Wunsch alignment.
 
@@ -199,7 +210,7 @@ def needleman_wunsch(seq1, seq2, identity_score, substitution_score, gap_score):
     alignment : tuple
         A tuple containing the aligned sequences and a score.
     """
-    matrix, arrow_matrix = fill_needleman_wunsch_matrix(seq1, seq2, identity_score, substitution_score, gap_score)
+    matrix, arrow_matrix = fill_needleman_wunsch_matrix(seq1, seq2, identity_score, substitution_score, gap_opening_score, gap_extension_score)
     
     score = matrix.at[len(seq1), len(seq2)]
 
@@ -225,3 +236,4 @@ def needleman_wunsch(seq1, seq2, identity_score, substitution_score, gap_score):
     print_nw_result(matrix, arrow_matrix, score, alignement1, alignement2, seq1, seq2)
 
     return score, alignement1, alignement2
+
